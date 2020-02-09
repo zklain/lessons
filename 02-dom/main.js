ef9d5c43b29f4780e6f6082320f88f7945a16322
zklain <zdnkk@protonmail.com>
2020-02-09 13:36:50 +0100

    hw02

---
+++ 02-dom/main.js
@@ -0,0 +1,31 @@
+// const addClass = (elem, class) => {
+// //   elem.classList.add(class);
+// // // };
+
+// const redButton = document.getElementById('makeRed');
+// const greenButton = document.getElementById('makeGreen');
+
+// redButton.addEventListener('click', () => {
+//   const nadpis = document.getElementById('heading');
+
+//   if (nadpis.classList.contains('green')) {
+//     nadpis.classList.remove('green');
+//   }
+//   nadpis.classList.toggle('red');
+// });
+
+// greenButton.addEventListener('click', () => {
+//   document.getElementById('heading').classList.toggle('green');
+// });
+
+const arr = [12, 12, 12, 12];
+
+const res = arr.map(elem => elem ** 2);
+
+'helloo'.spl;
+
+console.log(res);
+
+const base = document.getElementsByName('base')[0].value;
+const power = document.getElementsByName('power')[0].value;
+console.log(base ** power);
